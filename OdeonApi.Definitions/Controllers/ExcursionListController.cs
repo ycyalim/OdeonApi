bf4e23995c1d0bdb6c051417811b436f97dd82f1
ycyalim <ycyalim@hotmail.com>
2020-12-27 18:03:45 +0200

    adasdadaadas

--- OdeonApi.Definitions/Controllers/ExcursionListController.cs
+++ OdeonApi.Definitions/Controllers/ExcursionListController.cs
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,3 +21,49 @@ namespace OdeonApi.Definitions.Controllers
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeonApi.Definitions.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExcursionListController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<string> Get(int countryId,string languageCode)
+        {
+            return new List<string>() { "Antalya", "Muğla", "İzmir" };
+        }
+    }
+}
+>>>>>>> 8e7e67b539f633f81d8b86aacc0abc8e57af6de9
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeonApi.Definitions.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExcursionListController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<string> Get(int countryId,string languageCode)
+        {
+            return new List<string>() { "Antalya", "Muğla", "İzmir" };
+        }
+    }
+}
+>>>>>>> 8e7e67b539f633f81d8b86aacc0abc8e57af6de9
